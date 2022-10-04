dbad26e88a3e38331d63fb424b9e8a4fa584e8a5
Dipam Bhuyan <81673711+DipamBhuyan@users.noreply.github.com>
2022-10-04 04:48:19 -0700

    Add the script for the password saver

--- script.js
+++ script.js
@@ -22,3 +22,14 @@ function getDate(){
 
     document.getElementById("date").value = today.getFullYear() + '-' + ('0' + (today.getMonth() + 1)).slice(-2) + '-' + ('0' + today.getDate()).slice(-2);
 }
+
+//send the password through email
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbzBoAgAyWLCTUPBNfMXv9Ufd3ijOER0Szc2Gq02lDs-OlRn3AgrH9qshwdcoExnesxEGg/exec'
+    const form = document.forms['google-sheet']
+          
+    form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+    .catch(error => console.error('Error!', error.message))
+})
